8f1ffa41af8be4782c7fec257ed01e7081cadc08
Yuriy Kurbakov <k.blvrrr@yandex.ru>
2019-06-07 07:45:58 +0300

    added main components

---
+++ src/InputItem/InputItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InputItem = () => (
+  <div>
+    <input type="text" placeholder="What needs to be done?" size="32" />
+  </div>
+);
+
+export default InputItem;
---
+++ src/Item/Item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Item = () => (
+  <span>need to do</span>
+);
+
+export default Item;
